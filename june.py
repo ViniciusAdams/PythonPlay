fedd481d9c137a57c50f493b46d0c6836d5a649f
ViniciusAdams <viniciusadams98@gmail.com>
2026-06-22 21:10:53 +0100

    page 25 ended

---
+++ june.py
@@ -0,0 +1,17 @@
+#Variables and simple data types
+print("Hello Python world!")
+message = "Testing"
+print(message)
+message_2 = "Hello Python course reader!"
+print(message_2)
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+first_name = "vini"
+last_name = "adams"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+print ("Hello," + full_name.title()+ "!")
